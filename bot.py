af301ce188a8cc0099741d577e04b43f97b0786d
Josh Dudley <dudley.joshua789@gmail.com>
2021-06-05 01:43:02 -0500

    IT WORKS

--- bot.py
+++ bot.py
@@ -3,28 +3,63 @@ import discord
 intents = discord.Intents.default()
 intents.presences = True
 intents.members = True
-
+intents.voice_states = True
 client = discord.Client(intents=intents)
+
+ILC_guild_id = 705969305715474683
 
 @client.event
 async def on_ready():
     print("We have logged in as {0.user}\n".format(client))
-    await messageTrish()
-
-async def messageTrish():
-    for guild in client.guilds:
-        async for member in guild.fetch_members(limit=15):
-            if member.name  == "patcat":
-                channel = await member.create_dm()
-                message = input(str("uwu boy: "))
-                response = await member.send(message)
 
 @client.event
-async def on_message(message):
-    if not message.author.bot:
-        if message.author.name == "patcat":
-            print("uwu girl: " +  message.content)
-            await messageTrish()
+async def on_voice_state_update(member, before, after):
+
+    if before.channel is None and after.channel is not None:    ## "member" joins a voice channel
+        
+        print(member.name + " joined a call")
+        
+        ilc = client.get_guild(ILC_guild_id) 
+        channels = ilc.voice_channels                           ## grabs all channels in the server       
+        curr_users = []                                         ## all users CURRENTLY in a voice channel
+
+        for voice_channel in channels:                  
+            for user in voice_channel.members:
+                curr_users.append(user)
+
+        with open("people.txt", "r") as people_file:
+            while people_file:
+                line = people_file.readline().rstrip()                   ## current memeber.name 
+                curr_line_in_voice = False
+
+                for user in curr_users:                         ## if the user currently is not in a voice channel
+                    if user.name == line:
+                        curr_line_in_voice = True                        
+
+                if not curr_line_in_voice:
+                    async for user in ilc.fetch_members(limit=ilc.member_count):
+                        if user.name == line:
+                            await user.send(str(member) + " joined ILC")
+                            break
+                            
+
+
+
+
+
+
 
 client.run('NzY1NTA2MDE2NjY1Nzk2NjE4.X4VzCA.gYYWr-4tEu52LYlPnup_JKn3eF8')
 
+
+
+
+'''
+1. see if a person joined a voice_channel
+2. alert everyone that's either 
+    a. not in the call
+    b. not the same person
+
+    that a person joined the voice_channel
+
+'''
